5d0c3487f47ef9b4d8a4b80d0c5a7ffd2b5a6a19
Ernest Wong <t-chwong@microsoft.com>
2018-02-06 14:39:21 -0800

    Fix CI errors

--- scripts/ci/test_automation.sh
+++ scripts/ci/test_automation.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Running test_setup.sh...'
-. ./test_setup.sh
+. $TRAVIS_BUILD_DIR/scripts/ci/test_setup.sh
 
 echo 'Starting unit test...'
 python test/test_alias.py
--- scripts/ci/test_setup.sh
+++ scripts/ci/test_setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
-
 cd $TRAVIS_BUILD_DIR
 cd ..
 export HOME_DIR=$(pwd)
-echo 'Cloning Azure/azure-cli to $HOME_DIR'
-git clone https://github.com/Azure/azure-cli.git
+
+echo 'Cloning Azure/azure-cli...'
+git clone https://github.com/Azure/azure-cli.git $HOME_DIR/azure-cli
 
 echo 'Setting PYTHONPATH'
 # Set PYTHONPATH to point to azure-cli-core and azext_alias
-export PYTHONPATH=$HOME_DIR/azure-cli/src/azure-cli-core:$HOME_DIR/azure-cli-alias-extension/azext_alias:${PYTHONPATH}
+export PYTHONPATH=$HOME_DIR/azure-cli/src/azure-cli-core:$TRAVIS_BUILD_DIR/azext_alias:${PYTHONPATH}
 echo $PYTHONPATH
 
 echo 'Installing dependencies...'
-cd $TRAVIS_BUILD_DIR/azure-cli-alias-extension
+cd $TRAVIS_BUILD_DIR
 pip install -r requirements.txt
