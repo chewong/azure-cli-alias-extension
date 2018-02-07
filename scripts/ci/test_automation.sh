1d32dba83a3519827f29910a8df37ab8d90a02d4
Ernest Wong <t-chwong@microsoft.com>
2018-02-06 16:33:47 -0800

    Add color to Travis CI output

--- scripts/ci/test_automation.sh
+++ scripts/ci/test_automation.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-echo 'Running test_setup.sh...'
+##############################################
+# Define colored output func
+function title {
+    LGREEN='\033[1;32m'
+    CLEAR='\033[0m'
+
+    echo -e ${LGREEN}$1${CLEAR}
+}
+
+title 'Running test_setup.sh...'
 . $TRAVIS_BUILD_DIR/scripts/ci/test_setup.sh
 
-echo 'Starting unit test...'
+title 'Starting unit tests...'
 python test/test_alias.py
--- scripts/ci/test_setup.sh
+++ scripts/ci/test_setup.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 
+##############################################
+# Define colored output func
+function title {
+    LGREEN='\033[1;32m'
+    CLEAR='\033[0m'
+
+    echo -e ${LGREEN}$1${CLEAR}
+}
+
 cd $TRAVIS_BUILD_DIR
 cd ..
 export HOME_DIR=$(pwd)
 
-echo 'Cloning Azure/azure-cli...'
+title 'Cloning Azure/azure-cli...'
 git clone https://github.com/Azure/azure-cli.git $HOME_DIR/azure-cli
 
-echo 'Setting PYTHONPATH'
+title 'Setting PYTHONPATH...'
 # Set PYTHONPATH to point to azure-cli-core and azext_alias
 export PYTHONPATH=$HOME_DIR/azure-cli/src/azure-cli-core:$TRAVIS_BUILD_DIR/azext_alias:${PYTHONPATH}
-echo $PYTHONPATH
+title $PYTHONPATH
 
-echo 'Installing dependencies...'
+title 'Installing dependencies...'
 cd $TRAVIS_BUILD_DIR
 pip install -r requirements.txt
